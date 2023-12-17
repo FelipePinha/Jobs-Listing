a50a826714840763f918d5c3ebc14a7f9cc5933f
Felipe Carvalho <felipe_pc7@hotmail.com>
2023-12-17 13:57:16 -0300

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import ViteIcon from '../public/vite.svg';
+import ViteIcon from '/vite.svg';
+import './index.css';
 
 function App() {
     return (
-        <>
+        <div className="h-screen bg-slate-500">
             <img src={ViteIcon} alt="aaaa" />
-        </>
+        </div>
     );
 }
 
